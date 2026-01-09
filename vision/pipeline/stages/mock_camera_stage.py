82a9ed605cb198369653e48cc20ce4126ef31de8
Hussein EL AMINE <husseinelaminee@gmail.com>
2026-01-09 17:15:08 +0100

    [vision][stages] Add Mock camera stage for ci headless tests

---
+++ vision/pipeline/stages/mock_camera_stage.py
@@ -0,0 +1,15 @@
+from utils.path import SNAPSHOT_DIR
+from vision.pipeline.stages.stage_base import Stage
+import cv2, random
+
+class MockCameraStage(Stage):
+    def __init__(self):
+        self.paths = [
+            SNAPSHOT_DIR / "hand1.png",
+            SNAPSHOT_DIR / "no_hand.png",
+        ]
+        self.frames = [cv2.imread(str(p)) for p in self.paths]
+
+    def process(self, frame, state):
+        frame = random.choice(self.frames)
+        return frame, state
