f1c3b59de894a77608ea6ebd5958db04ac2fd20f
Hussein EL AMINE <husseinelaminee@gmail.com>
2026-01-11 19:23:52 +0100

    [vision][pipeline] Adapt pipeline to strongly typed state

--- vision/pipeline/pipeline.py
+++ vision/pipeline/pipeline.py
@@ -1,19 +1,21 @@
 from vision.pipeline.stages.stage_base import Stage
+from vision.pipeline.state import PipelineState
 
 class Pipeline:
     def __init__(self, stages:list[Stage]):
         self.stages:list[Stage] = stages
+        self.state = PipelineState()
 
     def initialize(self):
         for stage in self.stages:
             stage.initialize()
 
-    def process(self, frame, state):
+    def process(self, frame):
         for stage in self.stages:
-            frame, state = stage.process(frame, state)
+            frame, self.state = stage.process(frame, self.state)
             if frame is None:
-                return None, state
-        return frame, state
+                return None, self.state
+        return frame
 
     def attach_ui(self):
         for stage in self.stages:
