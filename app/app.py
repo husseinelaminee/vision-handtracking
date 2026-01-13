ae3aec8655ab7ff62b30e9fa25a60fe43ee354f6
Hussein EL AMINE <husseinelaminee@gmail.com>
2026-01-13 20:09:33 +0100

    [app] Adapt app to new structure and use a Single pipeline instead of a list

--- app/app.py
+++ app/app.py
@@ -1,20 +1,15 @@
 from app.renderer import Renderer
 from vision.pipeline.pipeline import Pipeline
-class Application:
-    def __init__(self, pipelines:list[Pipeline], renderer: Renderer):
-        self.pipelines: list[Pipeline] = pipelines
-        self.renderer: Renderer = renderer
-        for p in self.pipelines:
-            p.initialize()
-        
 
+class Application:
+    def __init__(self, pipeline: Pipeline, renderer: Renderer):
+        self.pipeline = pipeline
+        self.renderer = renderer
+        self._initialized = False
 
     def run(self, frames=0):
-        self.renderer.start()
-        for p in self.pipelines:
-            p.attach_ui()
-        
-
+        if not self._initialized:
+            raise Exception("Application should be initialized before run")
         if self.renderer.headless:
             limit = frames or 1
             for _ in range(limit):
@@ -28,10 +23,12 @@ class Application:
 
         self.renderer.close()
 
+    def initialize(self):
+        self._initialized = True
+        self.renderer.start()
+        self.pipeline.initialize()
+
     def _update_once(self):
         frame = None
-
-        for p in self.pipelines:
-            frame = p.process(frame)
-
+        frame = self.pipeline.process(frame)
         self.renderer.update(frame)
