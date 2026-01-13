ddca976d556d2aafb6c05882b9da6fd98621defe
Hussein EL AMINE <husseinelaminee@gmail.com>
2026-01-13 20:05:24 +0100

    [vision][camera] Fix manager camera listing bug

--- vision/camera/camera_manager.py
+++ vision/camera/camera_manager.py
@@ -9,22 +9,36 @@ class CameraManager:
         self.current_index = None
         self.pending_index = None
         self.name_index = None
+        self.indices: list[int] = None
 
-    def list_indices(self):
-        return list_cameras()
-
+    def list_indices_once(self):
+        if self.indices is None:
+            self.indices = list_cameras()
+        return self.indices
+    
     def get_camera_names(self):
-        return [f"Camera {i}" for i in self.list_indices()]
+        return [f"Camera {i}" for i in self.list_indices_once()]
+    
 
     def request_change(self, index):
+        indices = self.list_indices_once()
+
         if index == -1:
-            index = self.list_indices()[-1]
-            self.name_index = max(0,len(self.list_indices())-1)
-        else:
-            for index_key, index_val in enumerate(self.list_indices()):
-                index_val == index
-                self.name_index = index_key
+            # prendre la dernière caméra détectée
+            index = indices[-1]
+
+        # Vérifie que l’index OS existe vraiment
+        if index not in indices:
+            print(f"[CameraManager] Invalid OS camera index: {index}")
+            return
+
+        # Trouver la position UI correspondant à l'OS index
+        self.name_index = indices.index(index)
+
+        # Déclenche le changement réel (dans apply_change)
         self.pending_index = index
+
+
 
     def apply_change(self):
         if self.pending_index is None:
@@ -32,7 +46,6 @@ class CameraManager:
 
         if self.camera:
             self.camera.release()
-
         self.camera = Camera(self.pending_index)
         self.camera.open()
         self.current_index = self.pending_index
