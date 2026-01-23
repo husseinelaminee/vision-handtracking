24a6a4b8c287ccf6cef74551fffa3a963cc52eaf
Hussein EL AMINE <husseinelaminee@gmail.com>
2026-01-23 12:39:43 +0100

    [core][event] Make subcriber self sufficient

--- core/event/subscriber.py
+++ core/event/subscriber.py
@@ -3,9 +3,11 @@ from core.event.event_type import EventType
 
 class Subscriber(ABC):
     
-    def __init__(self):
+    def __init__(self, event_bus=None):
         super().__init__()
-        self.event_bus: "EventBus" = None
+        from core.event.event_bus import EventBus
+        self.event_bus: EventBus = event_bus or EventBus()
+        self.event_bus.register_subscriber(self)
         
     @property
     @abstractmethod
