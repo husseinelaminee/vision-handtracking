06aad14300e12dbd5895bbb779a7a6407a424b2e
Hussein EL AMINE <husseinelaminee@gmail.com>
2026-01-23 12:37:53 +0100

    [vision][camera] Fix cv2 blocking camera on invalid indices

--- vision/camera/camera_utils.py
+++ vision/camera/camera_utils.py
@@ -1,12 +1,14 @@
 import cv2
 
-cv2.setLogLevel(0)
 
 def list_cameras(max_test=10):
     available = []
     for index in range(max_test):
-        cap = cv2.VideoCapture(index)
-        if cap.isOpened():
-            available.append(index)
-            cap.release()
+        try:
+            cap = cv2.VideoCapture(index, cv2.CAP_V4L2)
+            if cap.isOpened():
+                available.append(index)
+                cap.release()
+        except:
+            continue
     return available
